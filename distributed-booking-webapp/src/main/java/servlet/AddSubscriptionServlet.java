d7fdd9324d160ff5f78bdf8b798c1a50efc05948
Stepnod98 <stefanodugo1998@gmail.com>
2022-06-18 19:59:39 +0200

    Update AddSubscriptionServlet.java

--- distributed-booking-webapp/src/main/java/servlet/AddSubscriptionServlet.java
+++ distributed-booking-webapp/src/main/java/servlet/AddSubscriptionServlet.java
@@ -45,7 +45,7 @@ public class AddSubscriptionServlet extends HttpServlet {
                 System.out.println("Receiving the new subscription info...");
 
                 int beachId = Integer.parseInt(request.getParameter("beachId"));
-                String type = request.getParameter("subType");
+                String type = request.getParameter("subTypes");
                 String user = session.getAttribute("user").toString();
                 String targetJSP;
 
